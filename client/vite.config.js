c620ef31ccc623ed93050c300a2ff1489ea1854e
Will Sullivan <wsulliv8@users.noreply.github.com>
2025-08-02 20:46:43 -0700

    Remove HTTPS from client

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "node:url";
-import fs from "fs";
+// import fs from "fs";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    https: {
-      key: fs.readFileSync("../server/server.key"),
-      cert: fs.readFileSync("../server/server.cert"),
-    },
+    // https: {
+    //   key: fs.readFileSync("../server/server.key"),
+    //   cert: fs.readFileSync("../server/server.cert"),
+    // },
     port: 5173,
   },
   resolve: {
